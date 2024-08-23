3c2523f4d2675c196f07a3d75fc4e3f231517741
Marco <mbarozzi@gmail.com>
2024-08-23 05:40:49 -0300

    compomente category

---
+++ lib/utils/consts/categories.dart
@@ -0,0 +1,9 @@
+const String imagePath ="'assets/images/";
+
+const Map<String,String> categoriesMap ={
+  'creatures':'Creatures',
+  'equipament':'Equipament',
+  'materials':'Materials',
+  'monsters':'Monsters',
+  'treasure':'Treasure',
+};
