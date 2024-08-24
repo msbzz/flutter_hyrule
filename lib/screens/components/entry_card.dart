97ea66cc72b0a4bc8c982782019905c454e30f5e
Marco <mbarozzi@gmail.com>
2024-08-24 03:40:12 -0300

    concluindo a remoção de um item em favoritos

--- lib/screens/components/entry_card.dart
+++ lib/screens/components/entry_card.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:hyrule/controllers/dao_controller.dart';
 import 'package:hyrule/domain/models/entry.dart';
 import 'package:hyrule/screens/datails.dart';
 
 class EntryCard extends StatelessWidget {
-  const EntryCard({Key? key, required this.entry,required this.isSaved}) : super(key: key);
+  EntryCard({Key? key, required this.entry,required this.isSaved}) : super(key: key);
   final Entry entry;
   final bool isSaved;
+
+  final DaoController _daoController = DaoController();
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Dismissible(
         direction: isSaved ? DismissDirection.endToStart:DismissDirection.none,
         key:ValueKey<int> (entry.id),
-        onDismissed: (direction) { },
+        onDismissed: (direction) { 
+           _daoController.deleteEntry(entry: entry);
+        },
         child: Column(children: <Widget>[
           InkWell(
             onTap: (){
